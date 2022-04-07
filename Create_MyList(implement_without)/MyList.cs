3b2b0fcb9e154ca893e221ad9b2f58648d7797ab
soner turan <sonerturan609@gmail.com>
2022-04-07 20:55:52 +0300

    Add project files.

---
+++ Create_MyList(implement_without)/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_MyList_implement_without_
+{
+    class MyList<T>//generic class
+    {
+        T[] items;
+        public MyList()//ctor
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;//geçici dizide elemanları tutuyoruz(temporary array)
+            items = new T[items.Length + 1];//dizinin eleman sayısını bir arttır
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length-1]=item;
+        }
+        public T[] GetItem()
+        {
+            return items;
+        }
+    }
+}
---
+++ Create_MyList(implement_without)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_MyList_implement_without_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> sayilar = new MyList<int>();
+            sayilar.Add(2);
+            sayilar.Add(3);
+            sayilar.Add(12);
+            foreach (var item in sayilar.GetItem())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
